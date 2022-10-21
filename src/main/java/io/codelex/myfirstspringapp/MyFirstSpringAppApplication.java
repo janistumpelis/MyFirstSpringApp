3c1246609c394674003a2991fdd9479cbb5fa3a2
Janis Tumpelis <jtumpelis@evolutiongaming.com>
2022-10-21 20:06:47 +0300

    Added empty project

---
+++ src/main/java/io/codelex/myfirstspringapp/MyFirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.myfirstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstSpringAppApplication.class, args);
+    }
+
+}
