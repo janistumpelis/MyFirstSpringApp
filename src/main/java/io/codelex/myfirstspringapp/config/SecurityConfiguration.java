83f9a4c09fbd720fef75a40db0a925b038d84cbd
Janis Tumpelis <jtumpelis@evolutiongaming.com>
2022-10-26 20:54:00 +0300

    Added security config

---
+++ src/main/java/io/codelex/myfirstspringapp/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package io.codelex.myfirstspringapp.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/spring/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        http.headers().frameOptions().sameOrigin();
+    }
+
+}
